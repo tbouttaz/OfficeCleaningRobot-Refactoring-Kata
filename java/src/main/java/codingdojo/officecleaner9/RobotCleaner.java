7b4c060c45a89b328a5b1035199671457f0a5f06
Tom Bouttaz <thomas_bouttaz@dell.com>
2025-03-10 11:07:13 +0000

    Revert "Extract Step To North to separate method"
    This reverts commit 5d95c1957af0676f11dd80520c66f23b3d697686.

--- java/src/main/java/codingdojo/officecleaner9/RobotCleaner.java
+++ java/src/main/java/codingdojo/officecleaner9/RobotCleaner.java
@@ -61,7 +61,7 @@ public class RobotCleaner {
     /**
      * Sorted List(for fast searching) of visited spaces(coordinates) on floor.
     */
-    public TreeMap<String, Coordinates> visitedPlaces = new TreeMap<String, Coordinates>(); //TODO Refactor: this could just be a Set
+    public TreeMap<String, Coordinates> visitedPlaces = new TreeMap<String, Coordinates>();
 
     /**
      * Constructor with no parameter
@@ -100,12 +100,33 @@ public class RobotCleaner {
         {
             case 'N':
                 for (int i = 0; i < steps; i++)
-                    processStepToNorth();
+                {
+                    //if robot is at the boundary of the floor it just stops there and wait for next direction 
+                    if (CURRENT_Y + 1 > FLOOR_UPPER_LENGTH)
+                    {
+                        CURRENT_Y = FLOOR_UPPER_LENGTH;
+                        break;
+                    }
+                    else
+                    {
+                        //if place is not visited
+                        if (!visitedPlaces.containsKey(String.format("%s %s", CURRENT_X, CURRENT_Y + 1)))
+                        {
+                            //Visit it and add to the visited places
+                            visitedPlaces.put(String.format("%s %s", CURRENT_X, CURRENT_Y + 1), new Coordinates(CURRENT_X, ++CURRENT_Y));
+                        }
+                        else
+                        {
+                            //otherwise moves to the next location
+                            ++CURRENT_Y;
+                        }
+                    }
+                }
                 break;
             case 'S':
                 for (int i = 0; i < steps; i++)
                 {
-                    //if robot is at the boundary of the floor it just stops there and wait for next direction
+                    //if robot is at the boundary of the floor it just stops there and wait for next direction 
                     if (CURRENT_Y - 1 < FLOOR_LOWER_LENGTH)
                     {
                         CURRENT_Y = FLOOR_LOWER_LENGTH;
@@ -178,26 +199,6 @@ public class RobotCleaner {
                 }
                 break;
         }
-    }
-
-    private void processStepToNorth() {
-        // if robot is at the boundary of the floor it just stops there and wait for next direction
-        if (isOutOfBoundWhenSteppingNorth()) {
-            CURRENT_Y = FLOOR_UPPER_LENGTH;
-        } else {
-            //if place is not visited
-            if (!visitedPlaces.containsKey(String.format("%s %s", CURRENT_X, CURRENT_Y + 1))) {
-                //Visit it and add to the visited places
-                visitedPlaces.put(String.format("%s %s", CURRENT_X, CURRENT_Y + 1), new Coordinates(CURRENT_X, ++CURRENT_Y));
-            } else {
-                //otherwise moves to the next location
-                ++CURRENT_Y;
-            }
-        }
-    }
-
-    private boolean isOutOfBoundWhenSteppingNorth() {
-        return CURRENT_Y + 1 > FLOOR_UPPER_LENGTH;
     }
 
     public void PrintVisitedPlaces()
